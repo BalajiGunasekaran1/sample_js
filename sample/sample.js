daf95afb8b8f55c7d8533e11a0eda88e36f1efae
BalajiGunasekaran1 <balajigunasekaran1@gmail.com>
2020-12-10 16:10:58 +0530

    sum and product

---
+++ sample/sample.js
@@ -0,0 +1,6 @@
+let first = parseInt(prompt("Enter the first value: ")) ;
+let second = parseInt(prompt("Enter the second value: ")) ;
+let sum = 0, product = 1;
+sum = first+second ;
+product = first*second;
+console.log('Sum = '+ sum + '\t Product = ' + product )
